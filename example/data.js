ec52848c5d6f3279286041e15d9726b56eea385a
Legodev <legodevgit@mailbox.org>
2018-09-17 16:15:41 +0200

    restored data.js for unhierarchical_data.html example

---
+++ example/data.js
@@ -0,0 +1,9 @@
+var unhierarchicalGanttData = [
+    {
+        series: [
+            { name: "Feature 1", start: new Date(2011,8,1), end: new Date(2011,8,3) },
+            { name: "Feature 2", start: new Date(2011,8,2), end: new Date(2011,8,5), color: "#f0f0f0" },
+            { name: "Feature 3", start: new Date(2011,8,1), end: new Date(2011,8,10) }
+        ]
+    }
+];
